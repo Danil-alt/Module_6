fc6f436b1a996ab0b0216c56dc1efc9f9e833ba5
Danil-alt <kilindany24@gmail.com>
2024-08-26 19:35:53 +0500

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,52 @@
+class Animal:
+    alive = True
+    fed = False
+    def __init__(self, name):
+        self.name = name
+
+
+class Plant:
+    edible = False
+    def __init__(self, name):
+        self.name = name
+    '''
+    alive - живой
+    fed - накормленность
+    ebidle - съедобный
+    '''
+class Mammal(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        elif food.edible:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+class Predator(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        elif food.edible:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+
+class Flower(Plant):
+    edible = True
+
+class Fruit(Plant):
+    edible = True
+
+q1 = Predator('Lion')
+q2 = Mammal('Horse')
+q3 = Fruit('Apple')
+q4 = Flower('Clover')
+
+print(q1.name)
+print(q3.name)
+print(q4.edible)
+print(q2.fed)
+q2.eat(q3)
+print(q2.fed)
